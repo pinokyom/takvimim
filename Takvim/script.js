eb371233f85e6309231759ae6f4146cf74ad3ff5
ზ ~̶  Ƥ!ƝØƘƳØM <45471512+pinokyom@users.noreply.github.com>
2025-10-23 23:38:48 +0300

    Update script.js

--- Takvim/script.js
+++ Takvim/script.js
@@ -1,8 +1,21 @@
+function openModal(date) {
+  selectedDate = date;
+  modalDate.textContent = `Not: ${date}`;
+
+  fetch(`https://takvimim.onrender.com/notes/${selectedDate}`)
+    .then(res => res.json())
+    .then(data => {
+      noteText.value = data.note || '';
+      doneCheckbox.checked = data.done || false;
+      modal.style.display = 'block';
+    });
+}
+
 function saveNote() {
   const note = noteText.value.trim();
   const done = doneCheckbox.checked;
 
-  fetch(`https://takvimim.onrender.com/${selectedDate}`, {
+  fetch(`https://takvimim.onrender.com/notes/${selectedDate}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ note, done })
@@ -12,26 +25,11 @@ function saveNote() {
   });
 }
 
-function openModal(date) {
-  selectedDate = date;
-  modalDate.textContent = `Not: ${date}`;
-
-  fetch(`https://takvimim.onrender.com/${selectedDate}`)
-    .then(res => res.json())
-    .then(data => {
-      noteText.value = data.note || '';
-      doneCheckbox.checked = data.done || false;
-      modal.style.display = 'block';
-    });
-}
-
 function deleteNote() {
-  fetch(`https://takvimim.onrender.com/${selectedDate}`, {
+  fetch(`https://takvimim.onrender.com/notes/${selectedDate}`, {
     method: 'DELETE'
   }).then(() => {
     generateCalendar();
     closeModal();
   });
 }
-
-
